17e860e0721eaf5bfab6d463bc0fcb3f6cafdc39
Micha Gorelick <mynameisfiber@gmail.com>
2016-08-09 13:36:36 -0400

    fancify

--- viewer.py
+++ viewer.py
@@ -2,11 +2,13 @@
 
 import sys
 import os
-import itertools as it
 import tty
 import termios
 import fcntl
 import struct
+
+import itertools as it
+import operator as op
 
 
 def getch():
@@ -21,30 +23,25 @@ def getch():
 
 
 def tab_sections(lines):
-    group = []
-    for line in lines:
-        if line:
-            group.append(line)
-        else:
-            if group:
-                yield group
-                group = []
-    if group:
-        yield group
+    """
+    Group lines of text into "paragraphs"... ie: sections separated by an empty
+    line
+    """
+    yield from map(list, map(op.itemgetter(1),
+                             it.groupby(lines, bool)))
 
 
 def group_viewport(groups, height):
-    cur_height = 0
-    cur_window = []
-    for group in groups:
-        cur_height += len(group) + 1
-        if cur_height > height:
-            yield cur_window
-            cur_window = []
-            cur_height = len(group) + 1
-        cur_window.extend(group)
-        cur_window.append('')
-    yield cur_window
+    """
+    Group sections of lines into chunks that are at most `height` long
+    """
+    total_height = 0
+    def grouper(g):
+        nonlocal total_height
+        total_height += len(g) + 1
+        return total_height // height
+    for key, group in it.groupby(groups, grouper):
+        yield list(it.chain.from_iterable(group))
 
 
 def chunker(seq, size):
@@ -52,7 +49,11 @@ def chunker(seq, size):
     yield from (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
-def two_column(*cols, full_width=80):
+def multicolumn(*cols, full_width=80):
+    """
+    Take multiple lists of strings and laminate them together, side by side,
+    for a multi-column view
+    """
     width = full_width // len(cols)
     for col_lines in it.zip_longest(*cols, fillvalue=''):
         yield ''.join(c.ljust(width) for c in col_lines[:-1]) +  \
@@ -60,9 +61,11 @@ def two_column(*cols, full_width=80):
 
 
 def terminal_size():
-    h, w, hp, wp = struct.unpack('HHHH',
+    h, w, hp, wp = struct.unpack(
+        'HHHH',
         fcntl.ioctl(0, termios.TIOCGWINSZ,
-        struct.pack('HHHH', 0, 0, 0, 0)))
+                    struct.pack('HHHH', 0, 0, 0, 0))
+    )
     return h, w
 
 
@@ -74,5 +77,5 @@ if __name__ == "__main__":
     ncols = max(1, width // max(map(len, lines)))
     for cols in chunker(group_viewport(groups, height), ncols):
         os.system("clear")
-        print("\n".join(two_column(*cols, full_width=width)))
+        print("\n".join(multicolumn(*cols, full_width=width)))
         getch()
